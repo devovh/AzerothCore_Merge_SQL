5c725a167fdb2682297a627a5e134c562d13ad64
devovh <devovh@gmail.com>
2022-10-23 22:50:44 +0200

    * Update.

--- data/sql/merge.bat
+++ data/sql/merge.bat
@@ -1,6 +1,8 @@
 copy /a ..\sql\base\db_world\*.sql /b world.sql
 copy /a ..\sql\base\db_characters\*.sql /b characters.sql
 copy /a ..\sql\base\db_auth\*.sql /b auth.sql
+copy /a ..\sql\base\db_custom\*.sql /b custom.sql
 copy /a ..\sql\updates\db_world\*.sql /b world_updates.sql
 copy /a ..\sql\updates\db_characters\*.sql /b characters_updates.sql
 copy /a ..\sql\updates\db_auth\*.sql /b auth_updates.sql
+copy /a ..\sql\updates\db_custom\*.sql /b custom_updates.sql
